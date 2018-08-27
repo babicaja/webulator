a7d440b496a824786273ba273ca63960bc818a5f
Dejan Babić <dejan.babic@kuca-soft.com>
2018-08-27 13:25:53 +0200

    Enabled dotenv functionality.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,3 +1,8 @@
 <?php
+// Set ROOT path
+define("__ROOT__", realpath(__DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR));
+
+// Load the environment variables.
+(new \Symfony\Component\Dotenv\Dotenv())->load(__ROOT__.DIRECTORY_SEPARATOR.".env");
 
 return new \Kucasoft\Application();
--- tests/Unit/CoreTest.php
+++ tests/Unit/CoreTest.php
@@ -9,7 +9,18 @@ class CoreTest extends BaseTest
     /** @test */
     public function it_can_bootstrap_the_application()
     {
-        $app = $this->bootstrap();
-        $this->assertInstanceOf(Application::class, $app, "The application could not be bootstrapped.");
+        $this->assertInstanceOf(Application::class, $this->app(), "The application could not be bootstrapped.");
+    }
+
+    /** @test */
+    public function it_has_the_app_name_dotenv_value()
+    {
+        $this->assertTrue(is_string(getenv("APP_NAME")), "The application name is not set.");
+    }
+
+    /** @test */
+    public function it_has_the_app_version_dotenv_value()
+    {
+        $this->assertTrue(is_string(getenv("APP_VERSION")), "The application version is not set.");
     }
 }
