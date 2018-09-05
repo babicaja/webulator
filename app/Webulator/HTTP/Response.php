deddf9328a9c6317239a435f5d73c16243a0074c
Dejan Babić <dejan.babic@kuca-soft.com>
2018-09-05 11:41:51 +0200

    Implementing parent constructor.

--- app/Webulator/HTTP/Response.php
+++ app/Webulator/HTTP/Response.php
@@ -7,4 +7,8 @@ use Zend\Diactoros\Response as ZendResponse;
 
 class Response extends ZendResponse implements WebulatorResponse
 {
+    public function __construct($body = 'php://memory', int $status = 200, array $headers = [])
+    {
+        parent::__construct($body, $status, $headers);
+    }
 }
