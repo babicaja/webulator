03910839ba5736351fa0b20415a871d90ddfb86f
dejan-babic <dejan.babic@kucasoft.com>
2018-09-21 16:57:39 +0200

    Cover root path helper with test.

---
+++ tests/Unit/HelpersTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class HelpersTest extends TestCase
+{
+    /** @test */
+    public function it_can_return_the_root_path()
+    {
+        $this->assertNotEmpty(rootPath());
+        $this->assertStringEndsWith("kucasoft\webulator", rootPath());
+    }
+
+    /** @test */
+    public function it_can_append_to_a_path()
+    {
+        $this->assertStringEndsWith("views", rootPath("views"));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
