395edaa057085fced2865246fb6533becc4bd779
dejan-babic <dejan.babic@kucasoft.com>
2018-09-26 13:32:07 +0200

    Introducing the Monolog logger wrapper.

---
+++ app/Webulator/Logger.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Webulator;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger as Monolog;
+use Webulator\Contracts\Configuration;
+use Webulator\Contracts\Logger as WebulatorLogger;
+
+class Logger extends Monolog implements WebulatorLogger
+{
+    /**
+     * Logger constructor.
+     *
+     * @param Configuration $configuration
+     * @throws \Exception
+     */
+    public function __construct(Configuration $configuration)
+    {
+        parent::__construct("webulator");
+
+        $storage = $configuration->get("storage.path");
+
+        $this->pushHandler(new StreamHandler($storage."/logs/webulator.log", Monolog::DEBUG));
+    }
+}
