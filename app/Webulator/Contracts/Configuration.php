b98c17666ee5a81002eeeffba9d34ab8cda2bb98
dejan-babic <dejan.babic@kucasoft.com>
2018-10-29 11:30:36 +0100

    Added missing dock block.

--- app/Webulator/Contracts/Configuration.php
+++ app/Webulator/Contracts/Configuration.php
@@ -4,7 +4,21 @@ namespace Webulator\Contracts;
 
 interface Configuration
 {
-    public static function load($path);
+    /**
+     * Sets the value for the given key.
+     *
+     * @param $key
+     * @param $value
+     * @return mixed
+     */
     public function set($key, $value);
+
+    /**
+     * Returns the value for the given key, or optionally the default value if no key is present.
+     *
+     * @param $key
+     * @param null $default
+     * @return mixed
+     */
     public function get($key, $default = null);
 }
