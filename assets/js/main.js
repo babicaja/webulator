2e52c0837b059f5770ba7d97765046a774338c9b
dejan-babic <dejan.babic@kucasoft.com>
2018-11-11 03:11:36 +0100

    Enabled babel/css/scss functionality in webpack.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+
 import webulator from './Webulator.js';
 
 let app = new webulator();
