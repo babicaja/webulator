6f619ee5f8c7f035dc72fd98c19761765d3cce64
dejan-babic <dejan.babic@kucasoft.com>
2018-11-15 22:43:29 +0100

    Middleware and Controllers resolve components with a help of a trait now.

--- app/Webulator/HTTP/Controllers/BaseController.php
+++ app/Webulator/HTTP/Controllers/BaseController.php
@@ -2,78 +2,9 @@
 
 namespace Webulator\HTTP\Controllers;
 
-use Webulator\Contracts\Configuration;
-use Webulator\Contracts\Database;
-use Webulator\Contracts\Logger;
-use Webulator\Contracts\Request;
-use Webulator\Contracts\Response;
-use Webulator\Contracts\RouteCollection;
-use Webulator\Contracts\Template;
-use Webulator\Contracts\HTTPClient;
+use Webulator\Traits\HasAppComponents;
 
 abstract class BaseController
 {
-    /**
-     * @var Configuration
-     */
-    protected $configuration;
-
-    /**
-     * @var Request
-     */
-    protected $request;
-
-    /**
-     * @var Response
-     */
-    protected $response;
-
-    /**
-     * @var RouteCollection
-     */
-    protected $route;
-
-    /**
-     * @var Template
-     */
-    protected $template;
-
-    /**
-     * @var Logger
-     */
-    protected $logger;
-
-    /**
-     * @var Database
-     */
-    protected $database;
-
-    /**
-     * @var HTTPClient
-     */
-    protected $HTTPClient;
-
-    /**
-     * BaseController constructor.
-     *
-     * @param Configuration $configuration
-     * @param Request $request
-     * @param Response $response
-     * @param RouteCollection $route
-     * @param Template $template
-     * @param Logger $logger
-     * @param Database $database
-     * @param HTTPClient $HTTPClient
-     */
-    public function __construct(Configuration $configuration, Request $request, Response $response, RouteCollection $route, Template $template, Logger $logger, Database $database, HTTPClient $HTTPClient)
-    {
-        $this->configuration = $configuration;
-        $this->request = $request;
-        $this->response = $response;
-        $this->route = $route;
-        $this->template = $template;
-        $this->logger = $logger;
-        $this->database = $database;
-        $this->HTTPClient = $HTTPClient;
-    }
+    use HasAppComponents;
 }
--- app/Webulator/Middleware/BaseMiddleware.php
+++ app/Webulator/Middleware/BaseMiddleware.php
@@ -3,30 +3,9 @@
 namespace Webulator\Middleware;
 
 use Webulator\Contracts\Middleware;
-use Webulator\Contracts\Request;
-use Webulator\Contracts\Response;
+use Webulator\Traits\HasAppComponents;
 
 abstract class BaseMiddleware implements Middleware
 {
-    /**
-     * @var Request
-     */
-    protected $request;
-
-    /**
-     * @var Response
-     */
-    protected $response;
-
-    /**
-     * BaseMiddleware constructor.
-     *
-     * @param Request $request
-     * @param Response $response
-     */
-    public function __construct(Request $request, Response $response)
-    {
-        $this->response = $response;
-        $this->request = $request;
-    }
+    use HasAppComponents;
 }
